304e01de7d02c76afc87a806cdb85e17229a3876
Quan <qqmquach@uwtaerloo.ca>
2026-07-11 07:58:34 -0400

    fix error

--- api/client.ts
+++ api/client.ts
@@ -1,16 +1,12 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || "https://oq4afpwtja.execute-api.us-east-1.amazonaws.com/prod";
 
 export async function request<T>(endpoint: string, options?: RequestInit): Promise<T> {
-    const url = `${API_BASE}${endpoint}`
-    console.log("URL =", url)
+    const url = `${API_BASE}${endpoint.startsWith("/") ? endpoint : `/${endpoint}`}`
     const res = await fetch(url, {
-        headers: {
-
-        },
         ...options,
     });
     if (!res.ok) {
-        throw new Error(`API Error: {res.status}`);
+        throw new Error(`API Error: ${res.status}`);
     }
     return res.json();
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic"
 import { Navigation } from "@/components/navigation"
 import { Hero } from "@/components/hero"
 import { Projects } from "@/components/projects"
