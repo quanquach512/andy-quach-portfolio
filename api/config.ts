53065a7135bbfec7fb663a4069be09dc2b96efb3
Quan <qqmquach@uwtaerloo.ca>
2026-07-12 13:17:44 -0400

    clean up

--- api/config.ts
+++ api/config.ts
@@ -1,3 +1,1 @@
-export const API_BASE =
-  process.env.NEXT_PUBLIC_API_URL ||
-  "https://oq4afpwtja.execute-api.us-east-1.amazonaws.com/prod"
+export const API_BASE = process.env.NEXT_PUBLIC_API_URL
