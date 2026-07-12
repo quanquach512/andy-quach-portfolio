b0c0abb052289f88eaa61821f1ddec74b81c60f5
Quan <qqmquach@uwtaerloo.ca>
2026-07-12 13:18:02 -0400

    clean up

--- lib/assets.ts
+++ lib/assets.ts
@@ -1,10 +1,11 @@
 const ASSET_CDN = process.env.NEXT_PUBLIC_ASSET_CLOUDFRONT_URL;
 
 export function asset(path: string) {
+  
   if (!path) return "";
   if (path.startsWith("http")) return path;
   if (path.startsWith("/")) {
-
+    console.log("URL = ", `${ASSET_CDN}${path}`)
     return `${ASSET_CDN}${path}`;
 
   }
